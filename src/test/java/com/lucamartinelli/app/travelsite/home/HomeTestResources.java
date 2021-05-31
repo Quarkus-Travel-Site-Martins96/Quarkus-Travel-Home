8db8d912fa262da40ebb1d52ae8c34bf3b91bcfd
Martins96 <luca.martinelli9696@gmail.com>
2021-05-31 19:02:15 +0200

    Added Junit tests

---
+++ src/test/java/com/lucamartinelli/app/travelsite/home/HomeTestResources.java
@@ -0,0 +1,9 @@
+package com.lucamartinelli.app.travelsite.home;
+
+import io.quarkus.test.common.QuarkusTestResource;
+import io.quarkus.test.derby.DerbyDatabaseTestResource;
+
+@QuarkusTestResource(DerbyDatabaseTestResource.class)
+public class HomeTestResources {
+
+}
