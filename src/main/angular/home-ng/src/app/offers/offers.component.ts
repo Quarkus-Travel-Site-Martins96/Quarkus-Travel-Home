12a3b85990b3349695709ab47886723b5258aaab
Martins96 <luca.martinelli9696@gmail.com>
2022-06-22 10:16:42 +0200

    Fixed missing properties in test env

--- src/main/angular/home-ng/src/app/offers/offers.component.ts
+++ src/main/angular/home-ng/src/app/offers/offers.component.ts
@@ -1,8 +1,8 @@
 import { Component, OnInit } from '@angular/core';
-import { Subscription } from 'rxjs';
+import { catchError, Subscription, throwError } from 'rxjs';
 import { RestService } from '../rest-service';
 import { Environment } from '../../environments/environment';
-import { HttpHeaders } from '@angular/common/http';
+import { HttpErrorResponse, HttpHeaders } from '@angular/common/http';
 import { OfferVO } from '../vo/offer-vo';
 
 
@@ -18,6 +18,7 @@ export class OffersComponent implements OnInit {
 	
 	offers: OfferVO = new OfferVO();
 	error: string;
+	isLoading: boolean;
 	
 	private sub: Subscription;
 
@@ -31,17 +32,33 @@ export class OffersComponent implements OnInit {
 			if (this.sub)
 				this.sub.unsubscribe();
 				
-			console.log("prendo le offerte");
+			console.log("Taking offerts via REST");
 				
+			this.isLoading = true;
+			this.error = undefined;
 			this.sub = this.rest.sendGet<OfferVO>(this.homeUrl, new HttpHeaders({
 				'content-type': 'application/json'
-			})).subscribe(r => {
-				this.offers = r.body;
-			}, error => {
-				console.error(error);
-				this.error = error;
+			})).pipe(
+				catchError(this.handleError)
+			).subscribe(r => {
+				this.isLoading = false;
+				console.log('Rest call finished');
+				if (!r || !r.body || r.body == null) {
+					this.error = 'Empty response for offers';
+				} else if (r.ok) {
+					this.offers = r.body;
+				}
 			});
 		}
 	}
+
+
+	private handleError(err: HttpErrorResponse) {
+		this.isLoading = false;
+		console.error(err);
+		this.error = err.message;
+		return throwError(() => new Error(this.error));
+	}
+
 
 }
