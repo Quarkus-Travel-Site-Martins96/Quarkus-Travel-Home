b07bbc79247beade3cdb7c5a65de74a87de488e4
Martins96 <luca.martinelli9696@gmail.com>
2022-07-18 23:43:57 +0200

    fixed bug logout for domain with more than one level

--- src/main/angular/home-ng/src/environments/environment.prod.ts
+++ src/main/angular/home-ng/src/environments/environment.prod.ts
@@ -42,7 +42,6 @@ export class Environment {
 	}
 	
 	public static getRegistrationHost(): string {
-		console.log("GAZZO1");
 		if (!this.singleton.registrationHost)
 			this.singleton.registrationHost = RestServiceEnvUrl.getHostUrl('registration');
 		return this.singleton.registrationHost;
